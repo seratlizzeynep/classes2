17a6efc387c7d2b145674eac7298c6123bab7597
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-09-19 21:37:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package classes2;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3, 4);
+		System.out.println(sonuc);
+	}
+
+}
